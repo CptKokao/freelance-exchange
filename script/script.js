09b3bafb22ae5422b6abf3781dd828bd11164005
Markov Sergey <markov@qtech.ru>
2019-12-18 18:33:52 +0300

    end day2

--- script/script.js
+++ script/script.js
@@ -7,6 +7,45 @@ document.addEventListener('DOMContentLoaded', () => {
     const blockFreelancer = document.querySelector('#block-freelancer');
     const blockChoice = document.querySelector('#block-choice');
     const btnExit = document.querySelector('#btn-exit');
+    const formCustomer= document.querySelector('#form-customer');
+    const orders = [];
+    const tableOrder = document.querySelector('#orders');
+    const modalRead = document.querySelector('#order_read');
+    const moadlActive= document.querySelector('#order_active');
+
+    // добавляет заказы в таблицу
+    let addOrder = () => {
+        tableOrder.textContent = "";
+        orders.forEach((el, index) => {
+            tableOrder.innerHTML += `
+            <tr class="order" data-number-order=${index}>
+                <td>${index + 1}</td>
+                <td>${el.title}</td>
+                <td class="${el.currency}"></td>
+                <td>${el.deadline}</td>
+            </tr>`
+        });
+    };
+
+    let openModal = (numberOrder) => {
+        const current = orders[numberOrder];
+        !current ? moadlActive.style.display = 'block' : modalRead.style.display = 'block';
+
+        const firstNameBlock= document.querySelector('.firstName'); 
+        const titleBlock= document.querySelector('.modal-title'); 
+        const emailBlock= document.querySelector('.email'); 
+        const descriptionBlock= document.querySelector('.description'); 
+        const dedlineBlock= document.querySelector('.dedline'); 
+        const currencyBlock= document.querySelector('.currency_img'); 
+        const countBlock= document.querySelector('.count'); 
+        const phoneBlock= document.querySelector('.phone'); 
+    }
+
+    // определяем target на всей таблице
+    tableOrder.addEventListener('click', (e) => {
+        const targetOrder = e.target.closest('.order');
+        openModal(targetOrder.dataset.numberOrder);
+    })
 
     customer.addEventListener('click', () => {
         blockCustomer.style.display = 'block';
@@ -16,6 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     freelancer.addEventListener('click', () => {
         blockFreelancer.style.display = 'block';
+        addOrder();
         blockChoice.style.display = 'none';
         btnExit.style.display = 'block';
     });
@@ -28,17 +68,14 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     // форма
-
-    const formCustomer= document.querySelector('#form-customer');
-    const orders = [];
-
     formCustomer.addEventListener('submit', (e) => {
         e.preventDefault();
         const obj = {};
 
-        [...formCustomer.elements].filter(function (el) {
+        // или Array.from(formCustomer.elements)
+        [...formCustomer.elements].forEach(function (el) {
             if ((el.tagName === 'INPUT' && el.type !== 'radio') || (el.checked) || (el.tagName === 'TEXTAREA')) {
-               return obj[el.name] = el.value;
+                obj[el.name] = el.value;
             }
         })
 
