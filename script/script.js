398d381bf9b4811262943ff33fe2522a47a85f64
Markov Sergey <markov@qtech.ru>
2019-12-19 18:46:35 +0300

    start day3

--- script/script.js
+++ script/script.js
@@ -12,13 +12,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const tableOrder = document.querySelector('#orders');
     const modalRead = document.querySelector('#order_read');
     const moadlActive= document.querySelector('#order_active');
+    
 
     // добавляет заказы в таблицу
-    let addOrder = () => {
+    let renderOrder = () => {
         tableOrder.textContent = "";
         orders.forEach((el, index) => {
             tableOrder.innerHTML += `
-            <tr class="order" data-number-order=${index}>
+            <tr class="order ${el.active ? 'new' : ''}" data-number-order=${index}>
                 <td>${index + 1}</td>
                 <td>${el.title}</td>
                 <td class="${el.currency}"></td>
@@ -26,46 +27,6 @@ document.addEventListener('DOMContentLoaded', () => {
             </tr>`
         });
     };
-
-    let openModal = (numberOrder) => {
-        const current = orders[numberOrder];
-        !current ? moadlActive.style.display = 'block' : modalRead.style.display = 'block';
-
-        const firstNameBlock= document.querySelector('.firstName'); 
-        const titleBlock= document.querySelector('.modal-title'); 
-        const emailBlock= document.querySelector('.email'); 
-        const descriptionBlock= document.querySelector('.description'); 
-        const dedlineBlock= document.querySelector('.dedline'); 
-        const currencyBlock= document.querySelector('.currency_img'); 
-        const countBlock= document.querySelector('.count'); 
-        const phoneBlock= document.querySelector('.phone'); 
-    }
-
-    // определяем target на всей таблице
-    tableOrder.addEventListener('click', (e) => {
-        const targetOrder = e.target.closest('.order');
-        openModal(targetOrder.dataset.numberOrder);
-    })
-
-    customer.addEventListener('click', () => {
-        blockCustomer.style.display = 'block';
-        blockChoice.style.display = 'none';
-        btnExit.style.display = 'block';
-    });
-
-    freelancer.addEventListener('click', () => {
-        blockFreelancer.style.display = 'block';
-        addOrder();
-        blockChoice.style.display = 'none';
-        btnExit.style.display = 'block';
-    });
-
-    btnExit.addEventListener('click', () => {
-        blockFreelancer.style.display = 'none';
-        blockCustomer.style.display = 'none';
-        blockChoice.style.display = 'block';
-        btnExit.style.display = 'none';
-    })
 
     // форма
     formCustomer.addEventListener('submit', (e) => {
@@ -76,11 +37,112 @@ document.addEventListener('DOMContentLoaded', () => {
         [...formCustomer.elements].forEach(function (el) {
             if ((el.tagName === 'INPUT' && el.type !== 'radio') || (el.checked) || (el.tagName === 'TEXTAREA')) {
                 obj[el.name] = el.value;
+                
             }
         })
+        obj.active = false;
 
         orders.push(obj);
         formCustomer.reset();
-        console.log(orders) 
+        // console.log(orders) 
     });
+        
+    const getOrder= document.querySelector('.get-order');
+    const modal = document.querySelector('.modal');
+
+    // открытие модального окна
+    let openModal = (numberOrder) => {
+
+        // выбираем из [] нужный объект по номеру массива, который мы передаем через numberOrder
+        const currentOrder = orders[numberOrder];
+
+        // добавляем все значения из объекта в переменные
+        let {title, firstName, email, phone, description, amount, currency, deadline, active} = currentOrder;
+
+        // выбираем какое модальное окно показать
+        const modal = active ? moadlActive : modalRead;
+        console.log(modal);
+        console.log(orders);
+
+        // получаем элементы модального окна
+        const firstNameBlock= modal.querySelector('.firstName'); 
+        const titleBlock= modal.querySelector('.modal-title'); 
+        const emailBlock= modal.querySelector('.email'); 
+        const descriptionBlock= modal.querySelector('.description'); 
+        const deadlineBlock= modal.querySelector('.deadline'); 
+        const currencyBlock= modal.querySelector('.currency_img'); 
+        const countBlock= modal.querySelector('.count'); 
+        const phoneBlock= modal.querySelector('.phone'); 
+        const capitulation= modal.querySelector('#capitulation'); 
+
+        // добавляем значения в верстку
+        titleBlock.textContent = title;
+        firstNameBlock.textContent = firstName;
+        emailBlock.innerText = email;
+        descriptionBlock.textContent = description;
+        deadlineBlock.textContent = deadline;
+        currencyBlock.classList.add(currency);
+        countBlock.textContent = amount;
+        if (modal === modalRead) {
+            phoneBlock.innerText = phone;
+        }
+
+        // открываем модальное окно
+        modal.style.display = 'block';
+
+        if (modal === moadlActive) {
+            // обработчик на кнопку getOrder
+            capitulation.addEventListener('click', (e) => {
+                modal.style.display = "none";
+                orders.splice(orders.indexOf(numberOrder), 1);
+                renderOrder();
+            });
+        }
+
+        
+        // обработчик на кнопку getOrder
+        getOrder.addEventListener('click', (e) => {
+            modal.style.display = "none";
+            currentOrder.active = true;
+            renderOrder();
+        });
+
+        // обработчик для закрытия модального окна
+        modal.addEventListener('click', (e) => {
+            const target = e.target;
+            const closeBtn = target.closest('.close');
+
+            // если клик по кнопки закрыть или по overflow то закрыть окно
+            if((closeBtn) || (target === modal))  {
+                modal.style.display = "none";
+            }
+        });
+    };
+
+    // определяем target на всей таблице
+    tableOrder.addEventListener('click', (e) => {
+        const target = e.target;
+        const targetOrder = target.closest('.order');
+        openModal(targetOrder.dataset.numberOrder);
+    });  
+
+    customer.addEventListener('click', () => {
+        blockCustomer.style.display = 'block';
+        blockChoice.style.display = 'none';
+        btnExit.style.display = 'block';
+    });
+
+    freelancer.addEventListener('click', () => {
+        blockFreelancer.style.display = 'block';
+        renderOrder();
+        blockChoice.style.display = 'none';
+        btnExit.style.display = 'block';
+    });
+
+    btnExit.addEventListener('click', () => {
+        blockFreelancer.style.display = 'none';
+        blockCustomer.style.display = 'none';
+        blockChoice.style.display = 'block';
+        btnExit.style.display = 'none';
+    })
 });
